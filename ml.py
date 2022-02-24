900d7facbaa01eb1a51ead22db0e09adda1ee805
matveynikon <66773302+matveynikon@users.noreply.github.com>
2022-02-24 18:32:04 +0100

    Update and rename main.py to ml.py

--- ml.py
+++ ml.py
@@ -15,12 +15,4 @@ features = list(zip(df["sepal_length"], df["sepal_width"]))
 
 model.fit(features,df["species"])
 
-# Saving model to current directory
-# Pickle serializes objects so they can be saved to a file, and loaded in a program again later on.
 pickle.dump(model, open('model.pkl','wb'))
-
-'''
-#Loading model to compare the results
-model = pickle.load(open('model.pkl','rb'))
-print(model.predict([[2.6, 8, 10.1]]))
-'''
