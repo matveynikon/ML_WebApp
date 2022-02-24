1e457b6e61ee0dcbd0d0db5010aea7b314bfa5c6
matveynikon <66773302+matveynikon@users.noreply.github.com>
2022-02-24 18:32:34 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,16 @@
-#import libraries
 import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
 
-#Initialize the flask App
 app = Flask(__name__)
 model = pickle.load(open('model.pkl', 'rb'))
 
-#default page of our web-app
 @app.route('/')
 def home():
     return render_template('index.html')
 
-#To use the predict button in our web-app
 @app.route('/predict',methods=['POST'])
 def predict():
-    '''
-    For rendering results on HTML GUI
-    '''
     int_features = [float(x) for x in request.form.values()]
     final_features = [np.array(int_features)]
     prediction = model.predict(final_features)
